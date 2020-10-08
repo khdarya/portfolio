6f53e545b95575cf08ec0946c6a7961ca7c5f25b
khdarya <darya.khakhlova@gmail.com>
2020-10-08 18:28:33 -0400

    Updates

---
+++ precache-manifest.61b6421d614f9ce1bb3799c66d66f46d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55de0e6b5bab42ac2d6afdf03d4d0566",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4bbbe7f8c6dbd968ee32",
+    "url": "/portfolio/static/css/main.78ddaf3c.chunk.css"
+  },
+  {
+    "revision": "8343cd71869a07d380d0",
+    "url": "/portfolio/static/js/2.b4b604e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.b4b604e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bbbe7f8c6dbd968ee32",
+    "url": "/portfolio/static/js/main.833abfaf.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
