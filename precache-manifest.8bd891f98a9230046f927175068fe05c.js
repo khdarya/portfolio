c02ba7793edbccea83a370d0826a421a20d7e4c1
khdarya <darya.khakhlova@gmail.com>
2020-11-22 22:14:17 -0500

    Updates

---
+++ precache-manifest.8bd891f98a9230046f927175068fe05c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5156cbf0c7541813d34fc7b425cd3601",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "69ffc5d46491248713d5",
+    "url": "/portfolio/static/css/main.3603eb18.chunk.css"
+  },
+  {
+    "revision": "f5ee57ce25ce6f7fd0be",
+    "url": "/portfolio/static/js/2.4af9ec24.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/portfolio/static/js/2.4af9ec24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69ffc5d46491248713d5",
+    "url": "/portfolio/static/js/main.5df3bc7a.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "2675c26677624059767ff756c91b6b6c",
+    "url": "/portfolio/static/media/calendar-in-docs-sheets-header.2675c266.jpg"
+  },
+  {
+    "revision": "905a738a915162d04d1581fb30b1480e",
+    "url": "/portfolio/static/media/sn1.905a738a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.af256e1298fade5740db406afded2bcb.js"
+  "/portfolio/precache-manifest.8bd891f98a9230046f927175068fe05c.js"
 );
 
 self.addEventListener('message', (event) => {
