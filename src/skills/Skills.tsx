4e8cb3cf2f0314f5dc6551313a3feca7fb212b60
khdarya <darya.khakhlova@gmail.com>
2021-01-10 19:07:05 -0500

    3.1 mainComponent

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -6,6 +6,9 @@ import Title from "../common/components/title/Title";
 import {faSass} from '@fortawesome/free-brands-svg-icons';
 import {faJs} from '@fortawesome/free-brands-svg-icons';
 import {faReact} from '@fortawesome/free-brands-svg-icons';
+import {faBootstrap} from '@fortawesome/free-brands-svg-icons';
+import {faVuejs} from '@fortawesome/free-brands-svg-icons';
+import {faNodeJs} from '@fortawesome/free-brands-svg-icons';
 
 
 
@@ -20,6 +23,12 @@ function Skills() {
                     <Skill fa={faSass} title={"Css"} description={"Web development skills with HTML/HTML5, CSS/CSS3, Less/Sass, Bootstrap. Experience in responsive web pages design with Media Queries, CSS Grid Layout and Flexbox"}/>
                     <Skill fa={faReact} title={"React"}
                            description={"Confident understanding of the concept of react-redux architecture. Experience in using Redux to manage the application state and applying middleware such as redux-promise, redux-thunk and redux-saga."}/>
+                <Skill fa={faBootstrap} title={"Bootstrap"} description={""} />
+                <Skill fa={faReact} title={"Redux"} description={""} />
+                <Skill fa={faReact} title={"TS"} description={""} />
+                <Skill fa={faVuejs} title={"Vue.js"} description={""} />
+                <Skill fa={faNodeJs} title={"Node.js"} description={""} />
+
                 </div>
             </div>
         </div>
