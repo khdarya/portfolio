b3810711a82d1e563dacc028b4ddd3e7c657298c
khdarya <darya.khakhlova@gmail.com>
2021-01-13 21:29:33 -0500

    Updates

--- precache-manifest.751767238f91857274c5f64cac40f84e.js
+++ precache-manifest.751767238f91857274c5f64cac40f84e.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "eaf96e7cd18c84087e0cc972c79d2d9f",
+    "revision": "288b75c26b2c23494e9b1c558de948a1",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "535550e1eb005e471bf3",
-    "url": "/portfolio/static/css/main.0bef0a84.chunk.css"
+    "revision": "a7b1d9acdfad447afb04",
+    "url": "/portfolio/static/css/main.0cad0815.chunk.css"
   },
   {
     "revision": "5d6c254b79412a196006",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.dd6b135c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "535550e1eb005e471bf3",
-    "url": "/portfolio/static/js/main.cd2980f8.chunk.js"
+    "revision": "a7b1d9acdfad447afb04",
+    "url": "/portfolio/static/js/main.b8860060.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
@@ -26,6 +26,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "2675c26677624059767ff756c91b6b6c",
     "url": "/portfolio/static/media/calendar-in-docs-sheets-header.2675c266.jpg"
+  },
+  {
+    "revision": "6c5a77aef9030c2896204b3706bf70f9",
+    "url": "/portfolio/static/media/photo.6c5a77ae.jpg"
   },
   {
     "revision": "905a738a915162d04d1581fb30b1480e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a80b56e428e0b351cb854370c996c742.js"
+  "/portfolio/precache-manifest.751767238f91857274c5f64cac40f84e.js"
 );
 
 self.addEventListener('message', (event) => {
