90917c16896fc217ec304fa027fcba0206ff3e79
khdarya <darya.khakhlova@gmail.com>
2021-01-20 22:52:37 -0500

    3.3 mainPage fix

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import style from './Main.module.scss';
 import styleContainer from './../common/styles/Container.module.css';
-import { Fade } from "react-awesome-reveal";
+import {Fade} from "react-awesome-reveal";
 import ReactTypingEffect from 'react-typing-effect';
 
 function Main() {
     return (
         <div className={style.mainBlock}>
-            <div className={styleContainer.container} >
+            <div className={styleContainer.container}>
                 <Fade>
                     <div className={style.greeting}>
                         <span className={style.hiText}>Hi There</span>
                         <span>I am Darya <span>Khakhlova</span></span>
-                        {/*<h1>A Front-End Developer</h1>*/}
-                        <ReactTypingEffect text="A Front-End Developer" />
+                        <ReactTypingEffect text="A Front-End Developer"/>
                     </div>
                 </Fade>
-
-            <div className={style.photo}>
-                <div className={style.image}>
-
+                <div className={style.photo}>
+                    <div className={style.image}>
+                    </div>
                 </div>
-            </div>
             </div>
         </div>
     );
